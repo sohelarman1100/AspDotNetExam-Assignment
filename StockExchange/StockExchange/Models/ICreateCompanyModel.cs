4fb706448953c2468e02dd3d477c7da2db3dbd63
sohelarman1100 <armanhossain54546@gmail.com>
2021-09-02 02:50:14 +0600

    Added Assignment-4 solution

---
+++ StockExchange/StockExchange/Models/ICreateCompanyModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockExchange.Models
+{
+    public interface ICreateCompanyModel
+    {
+        public string TradeCode { get; set; }
+        public void CreateCompany();
+    }
+}
